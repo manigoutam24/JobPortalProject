f360082213ce8dad1c8982bed9cf80e2be8a7604
manigoutam24 <goutammani82@gmail.com>
2026-08-12 23:31:25 +0530

    Working on Company Module

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -51,4 +51,134 @@ const getCompany = async (req, res) => {
   }
 };
 
-module.exports = { createCompany, getCompany };
+const getCompanyById = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+
+    const company = await companyModel.findById(companyId);
+
+    if (!company) {
+      return res.status(404).send({
+        success: false,
+        message: "Company not found",
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Company fetched Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).send({
+      success: false,
+      message: "Error in ID API",
+    });
+  }
+};
+
+const updateCompany = async (req, res) => {
+  try {
+    //! Fetch data from body and params
+    const { companyName, description, website, location } = req.body;
+    const { companyId } = req.params;
+
+    //! validate companyID
+    if (!companyId) {
+      return res.status(400).send({
+        success: false,
+        message: "Company ID is required",
+      });
+    }
+
+    //!FETCH COMPANY
+    const company = await companyModel.findById(companyId);
+    //! VALIDATE cOMPANY
+    if (!company) {
+      return res.status(404).send({
+        success: false,
+        message: "Company not found",
+      });
+    }
+
+    console.log("company.createdBy:", company.createdBy);
+    console.log("req.user:", req.user);
+    console.log("req.user._id:", req.user?._id);
+
+    //! CHECK OWNERSHIP BETWEEN CREATEDBY AND EXISTING USER
+    if (!company.createdBy.equals(req.user._id)) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized to update this company",
+      });
+    }
+
+    //?FETCH DATA AND UPDATE BY ID
+    const comapnyUpdate = await companyModel.findByIdAndUpdate(
+      companyId,
+      req.body,
+    );
+
+    return res.status(200).send({
+      success: true,
+      message: "Company updated Successfully",
+      comapnyUpdate,
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).send({
+      success: false,
+      message: "Error in Company Update API",
+    });
+  }
+};
+
+const deleteCompany = async (req, res) => {
+  try {
+    const { companyId } = req.params;
+    if (!companyId) {
+      return res.status(400).send({
+        success: false,
+        message: "Company id required",
+      });
+    }
+
+    const company = await companyModel.findById(companyId);
+    if (!company) {
+      return res.status(404).send({
+        success: false,
+        message: "Company not found",
+      });
+    }
+
+    if (!company.createdBy.equals(req.user._id)) {
+      return res.status(403).send({
+        success: false,
+        message: "You are not authorized to delete this company",
+      });
+    }
+    const deleteCompany = await companyModel.findByIdAndDelete(companyId);
+
+    return res.status(200).send({
+      success: true,
+      message: "Company delete Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).send({
+      success: false,
+      message: "Error in delete API",
+    });
+  }
+};
+
+module.exports = {
+  createCompany,
+  getCompany,
+  getCompanyById,
+  updateCompany,
+  deleteCompany,
+};
--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -3,10 +3,16 @@ const isAuthenticated = require("../middlewares/isAuthenticated");
 const {
   createCompany,
   getCompany,
+  getCompanyById,
+  updateCompany,
+  deleteCompany
 } = require("../controllers/companyController");
 const router = express.Router();
 
 router.post("/create", isAuthenticated, createCompany);
 router.get("/my", isAuthenticated, getCompany);
+router.get("/:companyId", isAuthenticated, getCompanyById);
+router.put("/:companyId", isAuthenticated, updateCompany);
+router.delete("/:companyId",isAuthenticated, deleteCompany)
 
 module.exports = router;
